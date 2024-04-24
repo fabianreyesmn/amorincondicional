321e1166ec7bc64eebd0ed2980b2aa4a0a91d282
JC <moraleschavezjuan@gmail.com>
2024-04-24 17:03:10 -0600

    primer borrador del recursosEductativos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecursosEducativosComponent } from './recursos-educativos/recursos-educativos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecursosEducativosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
