6b0af13a7b2c4de4c545cddc57cdd67e9aeb3c3b
Cesar Reyes Barrientos <151742492+CesarReyesBarrientos@users.noreply.github.com>
2024-04-26 17:49:18 -0600

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { NuestrosPerrosComponent } from './nuestros-perros/nuestros-perros.component';
-
-export const routes: Routes = [
-    { path: 'nuestros-perros', component: NuestrosPerrosComponent }
-];
