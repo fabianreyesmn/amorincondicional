b8a742bb5884e11f5cad80fbcd795aa36e50c966
Fabián Reyes <fabianreyesmn@gmail.com>
2024-04-26 21:50:07 -0600

    Mascotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { InicioComponent } from './Components/inicio/inicio.component';
 // import { AboutUsComponent } from './Components/about-us/about-us.component';
-// import { PerrosyGatosComponent } from './Components/perrosy-gatos/perrosy-gatos.component';
 // import { AyudaComponent } from './Components/ayuda/ayuda.component';
 import { ContactoComponent } from './Components/contacto/contacto.component';
+import { NuestrosPerrosComponent } from './Components/nuestros-perros/nuestros-perros.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/contacto', pathMatch: 'full' },
   // { path: 'home', component:  InicioComponent}, 
   // { path: 'about', component: AboutUsComponent},
   // { path: 'ayuda', component:  AyudaComponent}, 
-  // { path: 'mascotas', component: PerrosyGatosComponent}, 
+  { path: 'mascotas', component: NuestrosPerrosComponent}, 
   { path: 'contacto', component: ContactoComponent },
 ];
 
---
+++ src/app/services/animales.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { take } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalesService {
+
+  urlAPI:string = "https://amorinc.free.beeceptor.com/todos";
+
+  constructor(private http: HttpClient) { }
+
+  retornar(){
+    return this.http.get(this.urlAPI).pipe(take(1));
+  }
+}
